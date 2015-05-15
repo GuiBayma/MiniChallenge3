c526f51115fc1f6f3d0f6f4fd917e9c77b3d3e5b
Rafael Souza Belchior da Silva <rafael102@gmail.com>
2015-05-15 09:27:55 -0300

    Entities

---
+++ Vestibular.swift
@@ -0,0 +1,23 @@
+//
+//  Vestibular.swift
+//  MiniChallenge 3
+//
+//  Created by Rafael Souza Belchior da Silva on 15/05/15.
+//  Copyright (c) 2015 Guilherme Bayma. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Vestibular)
+class Vestibular: NSManagedObject {
+
+    @NSManaged var dataInicioInsc: NSDate
+    @NSManaged var dataFimInsc: NSDate
+    @NSManaged var dataGabarito: NSDate
+    @NSManaged var dataChamada: NSDate
+    @NSManaged var dataProvas: NSDate
+    @NSManaged var detalhes: String
+    @NSManaged var faculdade: Faculdade
+
+}
