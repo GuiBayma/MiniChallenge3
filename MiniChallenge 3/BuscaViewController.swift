466347568482aeb2bb649ba7c1dc4a611c4ea4d8
Guilherme Bayma <gfbayma@gmail.com>
2015-05-15 14:45:41 -0300

    alterado icones. incluido classe de busca.

---
+++ MiniChallenge 3/BuscaViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BuscaViewController.swift
+//  MiniChallenge 3
+//
+//  Created by Guilherme Bayma on 5/15/15.
+//  Copyright (c) 2015 Guilherme Bayma. All rights reserved.
+//
+
+import UIKit
+
+class BuscaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func buscar(sender: UIButton) {
+        self.performSegueWithIdentifier("resultadoBusca", sender: self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
